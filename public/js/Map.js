49bd7c60b50014eaef30dfda7cccdce784301fd5
Lin Yan <lin.yan@utah.edu>
2017-11-09 18:45:17 -0700

    finish part of map and yearchart

--- public/js/Map.js
+++ public/js/Map.js
@@ -7,21 +7,53 @@ class Map {
         this.projection = d3.geoConicConformal().scale(150).translate([400, 350]);
 	this.allData = allData;
     }
-    /**
-     * Function that clears the map
-     */
-    clearMap() {
-
-
-    }
 
     /**
      * Update Map with info for a specific FIFA World Cup
      * @param wordcupData the data for one specific world cup
      */
-    updateMap() {
-
- 
+    updateMap(info, type, year) {
+	let selected_year = 2006;
+	console.log(year)
+	let domain = [];
+	let range =[];
+	switch(type){
+	case 'PT':
+	    domain = [0, 5000000, 10000000, 20000000, 40000000, 100000000, 1000000000];
+            range = ["#D2D8DE", "#fcbba1", "#fc9272", "#fb6a4a", "#de2d26", "#a50f15", "#860308"];
+	    break;
+	case 'BR':
+	    domain = [0, 10, 20, 30, 40, 50];
+	    
+            range = ["#c6dbef", "#9ecae1","#6baed6", "#3182bd", "#08519c", "#063e78"];
+	    break;
+	case 'DR':
+	    domain = [0, 5, 10, 15, 20, 25, 30];
+            range = ["#f7f7f7", "#d9d9d9", "#bdbdbd", "#969696", "#636363", "#252525"];
+	    break;
+	case 'LE':
+	    domain = [20, 30, 40, 50, 60, 70, 80];
+            range = ["#edf8fb", "#ccece6","#99d8c9","#66c2a4","#41ae76","#238b45","#005824"];
+	    break;
+	default:
+	    break;
+	}
+	this.colorScale = d3.scaleThreshold()
+	    .domain(domain)
+	    .range(range);
+	
+	let worldmap=this;
+	let map = d3.select("#map");
+	let c=map.selectAll(".countries");
+	let countries = map.selectAll(".countries")
+		.each(function(d){
+		    let country = d3.select(this)._groups[0];
+		    for(var i=0; i<info.length; i++)
+			if(country[0].getAttribute("class") == ("countries "+info[i]["Country Code"])){
+			    d3.select(this)
+				.style("fill",(worldmap.colorScale(parseInt(info[i][selected_year+" [YR"+selected_year+"]"]))));}
+			   
+		});
 	
     }
 
--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,45 @@
         coordi.drawCoord(cntrydata);
     });
 
+//ly 11/09
+let yearchart = new YearChart();
+let year = yearchart.update();
+
+let Population_total=[];
+let Birth_rate=[];
+let Death_rate=[];
+let Life_expectancy=[];
+d3.csv("data/global-population-estimates.csv", function(error, global_data){
+    //console.log(global_data)
+    for(let i =0; i< global_data.length; i++){
+	if (global_data[i]["Series Name"]=="Population, total")
+	    Population_total.push(global_data[i]);
+	if (global_data[i]["Series Name"]=="Birth rate, crude (per 1,000 people)")
+	    Birth_rate.push(global_data[i]);
+	if (global_data[i]["Series Name"]=="Death rate, crude (per 1,000 people)")
+	    Death_rate.push(global_data[i]);
+	if (global_data[i]["Series Name"]=="Life expectancy at birth, total (years)")
+	    Life_expectancy.push(global_data[i]);
+    }
+    let attribute = [];
+    document.getElementById("SelectedAttribute").onchange = function(){
+	attribute = document.getElementById('SelectedAttribute').value;
+	switch(attribute){
+	case 'anscombe_I':
+	    worldMap.updateMap(Population_total, "PT", year);
+	    break;
+	case 'anscombe_II':
+	    worldMap.updateMap(Birth_rate, "BR", year);
+	    break;
+	case 'anscombe_III':
+	    worldMap.updateMap(Death_rate, "DR", year);
+	    break;
+	case 'anscombe_IV':
+	    worldMap.updateMap(Life_expectancy, "LE", year);
+	    break;
+	default:
+	    break;
+	}
+    };
+	
+});
